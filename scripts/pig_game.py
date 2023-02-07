a24f57277e23e1a5e83d7422249bba02acd49478
Jonathan <yonilevi0013@gmail.com>
2023-02-08 00:15:28 +0200

    fixing python script

--- scripts/pig_game.py
+++ scripts/pig_game.py
@@ -1,14 +1,8 @@
 import os
 
-
-
 try:
     os.system("kubectl delete -f ../pigGame/pig_game_deploy.yaml")
-except:
-    os.system("kubectl create -f ../pigGame/pig_game_deploy.yaml")
-      
-
-try:
     os.system("kubectl delete -f ../pigGame/pig_game_service.yaml")
 except:
+    os.system("kubectl create -f ../pigGame/pig_game_deploy.yaml")
     os.system("kubectl create -f ../pigGame/pig_game_service.yaml") 
