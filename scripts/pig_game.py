a04ee60d5abf53804517c43517f5d7a121bb98b4
Jonathan <yonilevi0013@gmail.com>
2023-02-09 11:29:33 +0200

    fixing pig_gmae script

--- scripts/pig_game.py
+++ scripts/pig_game.py
@@ -3,6 +3,7 @@ import os
 try:
     os.system("kubectl delete -f k8s/pigGame/pig_game_deploy.yaml")
     os.system("kubectl delete -f k8s/pigGame/pig_game_service.yaml")
+    raise Exception("The deployment and service not found in the cluster")
 except:
     os.system("kubectl create -f k8s/pigGame/pig_game_deploy.yaml")
-    os.system("kubectl create -f k8s/pigGame/pig_game_service.yaml") 
+    os.system("kubectl create -f k8s/pigGame/pig_game_service.yaml")
